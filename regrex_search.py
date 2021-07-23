eeb4f9d8223a6ae66729534e19074b46599faeb4
My Nguyen <33769063+thaimynguyen@users.noreply.github.com>
2021-07-23 16:59:08 -0700

    Add files via upload

--- regrex_search.py
+++ regrex_search.py
@@ -11,7 +11,7 @@ Link: https://automatetheboringstuff.com/2e/chapter9/#calibre_link-322
 """
 Input:
 _ folder path
-_ regex pattern to search for ==> for multiple patterns?
+_ regex pattern to search for 
 
 Output:
 _ lines that have a string matching the regex pattern
@@ -29,33 +29,36 @@ import os, re
 
 # TO DO:
 
+def search_txt(folder, regex):
+    os.chdir(folder)
+    files = os.listdir(folder)
 
-while True:
+    txt_files = list(filter(lambda x: x.endswith(('.txt', '.TXT')), files))
+
+    for file in txt_files:
+        with open(file, 'r') as f:
+            lines = f.read().splitlines()
+            for line in lines:
+                if re.search(regex, line):
+                    print(f'"{line}" from "{file}" file')
+
+def prompt_input():
+    while True:
+        
+        # Input folder path
+        folder_path = input('Enter the absolute path of the folder that you want to search: \n')
+        #folder_path = r'C:\Users\My\Desktop\Python projects\Automate_The_Boring_Stuff_With_Python\RegexSearch'
+        # Check if folder path exists:
+        if os.path.exists(folder_path):
+            regex_pattern = input('Enter the regular expression pattern that you want to search:\n')
+            return folder_path, regex_pattern
+        else:
+            print("The path you entered is not valid!")
     
-    # Input folder path
-    folder_path = input('Enter the absolute path of the folder that you want to search: \n')
-    
-    # Check if folder path exists:
-    if os.path.exists(folder_path):
-        os.chdir(folder_path)
-        regex_pattern = input('Enter the regular expression pattern that you want to search:\n')
-        break
-    else:
-        print("The path you entered is not valid!")
         
 
-
-files = os.listdir(folder_path)
-
-txt_files = list(filter(lambda x: x.endswith(('.txt', '.TXT')), files))
-
-for file in txt_files:
-    with open(file, 'r') as f:
-        lines = f.read().splitlines()
-        for line in lines:
-            if regex_pattern in line:
-                print(f'Line "{line}" from "{file}" file')
-
+search_txt(*prompt_input())
+#search_txt(r'C:\Users\My\Desktop\Python projects\Automate_The_Boring_Stuff_With_Python\RegexSearch', "\d")
 
 
 # splitlines() will get a list of lines without "\n" 
