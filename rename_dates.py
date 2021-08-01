6718ed383df14aa263383c828f0d7eb372882ea0
My Nguyen <33769063+thaimynguyen@users.noreply.github.com>
2021-08-01 15:59:47 -0700

    Add files via upload

--- rename_dates.py
+++ rename_dates.py
@@ -1,7 +1,7 @@
 """
 Here’s what the program does:
 
-1. It searches all the filenames in the current working directory for American-style dates.
+1. It searches all the filenames in a directory for American-style dates.
 2. When one is found, it renames the file with the month and day swapped to make it European-style.
 
 ebook-link: https://automatetheboringstuff.com/2e/chapter10/#calibre_link-333
@@ -13,10 +13,12 @@ Assumptions:
 
 Questions:
 - How can we differentiate a date like 08-01-2020 to be American style or European style?
-- Date validation? 
+- Date validation? (number of days in a month, leap year)
 
 """
 import os, re
+from typing import List
+
 
 American_dates = re.compile(r"""^(.*?)                  # group1: before_text
                                 (0[1-9]|1[0-2])         # group2: month
@@ -27,13 +29,24 @@ American_dates = re.compile(r"""^(.*?)                  # group1: before_text
                                 (.*?)$                  # group7: after_text
                             """, re.VERBOSE)
 
-dir = os.chdir(r'C:\Users\My\Desktop\Python projects\Automate_The_Boring_Stuff_With_Python\Rename_dates')
-# dir = os.getcwd()
-files = os.listdir(dir)
+def prompt_input():
+  while True:
+    dir = input('Enter the absolute folder path to search: \n')
+    if os.path.isdir(dir):
+      os.chdir(dir)
+      files = os.listdir(dir)
+      return files
+    else:
+      print('Invalid folder path!')
 
-for file in files:
-    match = American_dates.search(file)
-    if match:
-        new_name = match.group(1) + match.group(4) + match.group(3) + match.group(2) + match.group(5) + match.group(6) + match.group(7)
-        print(f'File "{file}" has been renamed to"{new_name}."')
-        os.rename(file, new_name)
+def change_file_name(files: List):
+  for file in files:
+      match = American_dates.search(file)
+      if match:
+          new_name = match.group(1) + match.group(4) + match.group(3) + match.group(2) + match.group(5) + match.group(6) + match.group(7)
+          os.rename(file, new_name)
+          print(f'File "{file}" has been renamed to"{new_name}."')
+          
+
+change_file_name(prompt_input())
+#change_file_name(r'C:\Users\My\Desktop\Python projects\Automate_The_Boring_Stuff_With_Python\Rename_dates')
