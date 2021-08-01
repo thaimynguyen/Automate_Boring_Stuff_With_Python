f29acf98b0abb4863fb31b5a0baad2f726949656
My Nguyen <33769063+thaimynguyen@users.noreply.github.com>
2021-08-01 15:33:28 -0700

    Key concepts: Regular Expression, OS module

--- regrex_search.py
+++ regrex_search.py
@@ -1,33 +1,16 @@
 #! python 3.8
 
-
 """
 Requirement:
 _ Write a program that opens all .txt files in a folder and searches for any line that matches a user-supplied regular expression. 
 _ The results should be printed to the screen.
+
 Link: https://automatetheboringstuff.com/2e/chapter9/#calibre_link-322
 """
-
-"""
-Input:
-_ folder path
-_ regex pattern to search for 
-
-Output:
-_ lines that have a string matching the regex pattern
-_ from which textfile?
-
-Assumptions:
-_ Accept both .txt and .TXT files
-
-"""
-
 
 
 import os, re
 
-
-# TO DO:
 
 def search_txt(folder, regex):
     os.chdir(folder)
@@ -44,12 +27,12 @@ def search_txt(folder, regex):
 
 def prompt_input():
     while True:
-        
+  
         # Input folder path
         folder_path = input('Enter the absolute path of the folder that you want to search: \n')
         #folder_path = r'C:\Users\My\Desktop\Python projects\Automate_The_Boring_Stuff_With_Python\RegexSearch'
         # Check if folder path exists:
-        if os.path.exists(folder_path):
+        if os.path.isdir(folder_path):
             regex_pattern = input('Enter the regular expression pattern that you want to search:\n')
             return folder_path, regex_pattern
         else:
